0b45bdcaddcb81ea30b9d10e318193e6c6476686
Alfredo0911 <sc.alfredo@akebono-astra.co.id>
2024-12-24 16:16:10 +0700

    update bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './assets/css/custome-style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import { useEffect, useState } from "react";
 import { logout } from "./controller/authController";
