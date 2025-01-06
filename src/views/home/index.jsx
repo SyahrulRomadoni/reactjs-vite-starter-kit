394efc96f89445ed8ec63ac68660bf7d613d21fa
Alfredo0911 <sc.alfredo@akebono-astra.co.id>
2025-01-06 11:24:04 +0700

    update

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
     return (
-        <div>
-            <div className="card shadow mt-3 text-center">
+        <div className="d-flex justify-content-center align-items-center vh-100">
+            <div className="card shadow text-center w-50">
                 <div className="card-body">
-                    <h1 className="fw-bold">REACT JS</h1>
-                    <p className="mb-0">HOME</p>
-                    <Link to="/login" aria-current="page">Login</Link>
+                    <h5 className="fw-bold">REACTJS VITE</h5>
+                    <h1 className="fw-bold">Starter Kit</h1>
                     <br />
-                    <Link to="/register" aria-current="page">Register</Link>
+                    <Link className="btn btn-primary m-1" to="/login" aria-current="page">Login</Link>
+                    <Link className="btn btn-primary m-1" to="/register" aria-current="page">Register</Link>
                 </div>
             </div>
         </div>
